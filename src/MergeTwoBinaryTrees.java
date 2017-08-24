9419cc3f9e02fa58455c69f8ea504528ead2dfa7
wuwenbin1 <wuwenbin1@jd.com>
2017-08-24 10:11:40 +0800

    合并两个二叉树

---
+++ src/MergeTwoBinaryTrees.java
@@ -0,0 +1,30 @@
+public class MergeTwoBinaryTrees {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+        if (t1 == null && t2 != null) {
+            return t2;
+        }
+        if (t1 != null && t2 == null) {
+            return t1;
+        }
+        TreeNode treeNode = new TreeNode(0);
+        treeNode.val = t1.val + t2.val;
+        treeNode.left = mergeTrees(t1.left, t2.left);
+        treeNode.right = mergeTrees(t1.right, t2.right);
+        return treeNode;
+    }
+
+}
