c16c91e98d4e3f3e928488dee31c6e5131a6b00b
Monster <516094242@qq.com>
2017-09-06 13:41:29 +0800

    58. Length of Last Word

---
+++ src/LengthofLastWord.java
@@ -0,0 +1,16 @@
+public class LengthofLastWord {
+
+    public int lengthOfLastWord(String s) {
+        int tail = s.length() - 1;
+        int length = 0;
+        while (tail >= 0 && s.charAt(tail) == ' ') {
+            tail--;
+        }
+        while (tail > 0 && s.charAt(tail) != ' ') {
+            length++;
+            tail--;
+        }
+        return length;
+    }
+
+}
