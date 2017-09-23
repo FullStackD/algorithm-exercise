13a655b522565de8d3fe351cca9cf2b55a650214
Monster <516094242@qq.com>
2017-09-23 17:39:45 +0800

    563. Binary Tree Tilt

---
+++ src/BinaryTreeTilt.java
@@ -0,0 +1,20 @@
+public class BinaryTreeTilt {
+
+    private int result = 0;
+
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+
+    public int postOrder(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSum = postOrder(root.left);
+        int rightSum = postOrder(root.right);
+        result += Math.abs(leftSum - rightSum);
+        return leftSum + rightSum + root.val;
+    }
+
+}
