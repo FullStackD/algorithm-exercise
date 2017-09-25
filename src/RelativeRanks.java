19bfbae9a68c89159ce324996e2ee1bb65a7de4f
Monster <516094242@qq.com>
2017-09-25 14:22:59 +0800

    506. Relative Ranks

---
+++ src/RelativeRanks.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class RelativeRanks {
+
+    public String[] findRelativeRanks(int[] nums) {
+        int length = nums.length;
+        int[] tmpArr = new int[length];
+        for (int i = 0; i < length; i++) {
+            tmpArr[i] = nums[i];
+        }
+        Arrays.sort(tmpArr);
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            String rank;
+            if (i == length - 1) {
+                rank = "Gold Medal";
+            } else if (i == length - 2) {
+                rank = "Silver Medal";
+            } else if (i == length - 3) {
+                rank = "Bronze Medal";
+            } else {
+                rank = String.valueOf(length - i);
+            }
+            map.put(tmpArr[i], rank);
+        }
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            list.add(map.get(nums[i]));
+        }
+        return list.toArray(new String[length]);
+    }
+
+
+}
