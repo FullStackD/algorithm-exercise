0082801e57f1843265b3468c3d06d3b4bae7856a
Monster <516094242@qq.com>
2017-10-07 22:41:48 +0800

    415. Add Strings

---
+++ src/AddStrings.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Admin on 2017/10/7 0007.
+ */
+public class AddStrings {
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int tmp = 0;
+        while (i >= 0 && j >= 0) {
+            int sum = Character.getNumericValue(num1.charAt(i--)) + Character.getNumericValue(num2.charAt(j--)) + tmp;
+            tmp = sum / 10;
+            sb.insert(0, sum % 10);
+        }
+        while (i >= 0) {
+            int sum = Character.getNumericValue(num1.charAt(i--)) + tmp;
+            tmp = sum / 10;
+            sb.insert(0, sum % 10);
+        }
+        while (j >= 0) {
+            int sum = Character.getNumericValue(num2.charAt(j--)) + tmp;
+            tmp = sum / 10;
+            sb.insert(0, sum % 10);
+        }
+        if (tmp != 0) {
+            sb.insert(0, tmp);
+        }
+        return sb.toString();
+    }
+
+}
