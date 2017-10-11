d6e3e8ed3aca12a39a1cd4fd514149a38b5fc684
Monster <516094242@qq.com>
2017-10-11 18:23:05 +0800

    35. Search Insert Position

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+}
