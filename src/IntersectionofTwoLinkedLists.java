f0cbd2381ad648ccee97bd2d7dd4aa593749c927
Monster <516094242@qq.com>
2017-10-12 14:18:03 +0800

    160. Intersection of Two Linked Lists

---
+++ src/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,52 @@
+
+public class IntersectionofTwoLinkedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int ALen = getLen(headA);
+        int BLen = getLen(headB);
+        int i = Math.abs(ALen - BLen);
+        ListNode tmpA = headA;
+        ListNode tmpB = headB;
+        if (ALen > BLen) {
+            while (i != 0) {
+                tmpA = tmpA.next;
+                i--;
+            }
+        } else {
+            while (i != 0) {
+                tmpB = tmpB.next;
+                i--;
+            }
+        }
+        while (tmpA != null) {
+            if (tmpA.val == tmpB.val) {
+                return tmpA;
+            }
+            tmpA = tmpA.next;
+            tmpB = tmpB.next;
+        }
+        return null;
+    }
+
+    private int getLen(ListNode headB) {
+        ListNode tmp = headB;
+        int len = 0;
+        while (tmp != null) {
+            len++;
+            tmp = tmp.next;
+        }
+        return len;
+    }
+
+}
