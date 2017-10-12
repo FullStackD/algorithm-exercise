3de5f847710a00964123c26ecd3648363c043179
Monster <516094242@qq.com>
2017-10-12 14:34:44 +0800

    160. Intersection of Two Linked Lists

--- src/IntersectionofTwoLinkedLists.java
+++ src/IntersectionofTwoLinkedLists.java
@@ -49,4 +49,17 @@ public class IntersectionofTwoLinkedLists {
         return len;
     }
 
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
 }
