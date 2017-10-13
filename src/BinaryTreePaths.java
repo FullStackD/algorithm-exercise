7b48f04c96b1e51b4ebd81de5f1b036a9019322b
Monster <516094242@qq.com>
2017-10-13 17:06:06 +0800

    257. Binary Tree Paths

---
+++ src/BinaryTreePaths.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreePaths {
+
+    private List<String> result = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        dfs(root, new ArrayList<>());
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            result.add(generateStr(list));
+            list.remove(list.size() - 1);
+        } else {
+            dfs(root.left, list);
+            dfs(root.right, list);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    private String generateStr(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("->");
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+}
