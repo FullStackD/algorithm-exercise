c9e48f5b0d071ec2c794eaddb21bbf0aa003de53
Monster <516094242@qq.com>
2017-10-16 20:37:51 +0800

    643. Maximum Average Subarray I

---
+++ src/MaximumAverageSubarrayI.java
@@ -0,0 +1,20 @@
+public class MaximumAverageSubarrayI {
+
+    public double findMaxAverage(int[] nums, int k) {
+        int j = 0;
+        int max = Integer.MIN_VALUE;
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= k - 1) {
+                tmp += nums[i];
+                max = Math.max(max, tmp);
+                tmp -= nums[j];
+                j++;
+            } else {
+                tmp += nums[i];
+            }
+        }
+        return (double) max / k;
+    }
+
+}
