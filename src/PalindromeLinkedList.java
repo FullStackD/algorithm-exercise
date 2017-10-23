34c07da516e8532d50d8b5dd3fcd4b6f5da3d3bf
Monster <516094242@qq.com>
2017-10-23 21:21:20 +0800

    234. Palindrome Linked List

---
+++ src/PalindromeLinkedList.java
@@ -0,0 +1,50 @@
+/**
+ * 234. Palindrome Linked List
+ *
+ * @author Monster
+ * @date 2017/10/23
+ */
+public class PalindromeLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
