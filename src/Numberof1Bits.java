9184e0ed74d2d8183e2f318e8ba53b0d985c524a
Monster <516094242@qq.com>
2017-10-27 15:32:00 +0800

    191. Number of 1 Bits

---
+++ src/Numberof1Bits.java
@@ -0,0 +1,21 @@
+/**
+ * 191. Number of 1 Bits
+ *
+ * @author Monster
+ * @date 10/27/2017
+ */
+public class Numberof1Bits {
+
+    public int hammingWeight(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) != 0) {
+                result++;
+            }
+            n >>>= 1;
+        }
+        return result;
+    }
+
+}
+
