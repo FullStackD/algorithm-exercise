b67ecaca2dadb23667697208d45eadc88561d92f
Monster <516094242@qq.com>
2017-11-02 17:27:40 +0800

    686. Repeated String Match

---
+++ src/RepeatedStringMatch.java
@@ -0,0 +1,25 @@
+/**
+ * 686. Repeated String Match
+ *
+ * @author Monster
+ * @date 2017/11/2
+ */
+public class RepeatedStringMatch {
+
+    public int repeatedStringMatch(String A, String B) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < B.length()) {
+            sb.append(A);
+            count++;
+        }
+        if (sb.toString().contains(B)) {
+            return count;
+        }
+        if (sb.append(A).toString().contains(B)) {
+            return ++count;
+        }
+        return -1;
+    }
+
+}
