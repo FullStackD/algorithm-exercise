65c4e8c4fa4f2740583eaeaeb43a8729b3c85f7e
Monster <516094242@qq.com>
2017-11-25 12:51:45 +0800

    5. Longest Palindromic Substring

---
+++ src/LongestPalindromicSubstring.java
@@ -0,0 +1,34 @@
+/**
+ * Author: Admin
+ * Date: 2017/11/25 0025.
+ */
+public class LongestPalindromicSubstring {
+
+    private int maxLen;
+    private int start;
+
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            extendPalidrome(s, i, i);
+            extendPalidrome(s, i, i + 1);
+        }
+        return s.substring(start, start + maxLen);
+    }
+
+    private void extendPalidrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        if (maxLen < right - left - 1) {
+            maxLen = Math.max(maxLen, right - left - 1);
+            // get the index
+            start = left + 1;
+        }
+    }
+
+}
