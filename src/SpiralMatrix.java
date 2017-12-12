ec824b70a9e60fa5c53afbc1df61962008c5b306
Monster <516094242@qq.com>
2017-12-12 18:32:19 +0800

    54. Spiral Matrix

---
+++ src/SpiralMatrix.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 54. Spiral Matrix
+ *
+ * @author Monster
+ * @date 2017/12/12
+ */
+public class SpiralMatrix {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return list;
+        }
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j++) {
+                list.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j++) {
+                list.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j--) {
+                    list.add(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j--) {
+                    list.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin++;
+        }
+
+        return list;
+    }
+
+
+}
