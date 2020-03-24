075ad895502e47987a8f438d7a8c82e3420b0abc
Monster <516094242@qq.com>
2020-03-24 20:47:19 +0800

    面试题 17.16. 按摩师

---
+++ src/leetcode/Massage.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class Massage {
+
+    /**
+     * 面试题 17.16. 按摩师
+     */
+    public int massage(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+}
