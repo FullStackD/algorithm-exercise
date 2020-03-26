66c62a42d0d7502c1917f3179094f2883dde68e6
Monster <516094242@qq.com>
2020-03-26 17:51:07 +0800

    14. 最长公共前缀

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+/**
+ * 14. 最长公共前缀
+ */
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
@@ -8,11 +11,13 @@ public class LongestCommonPrefix {
         }
         String str = strs[0];
         for (int i = 1; i < strs.length; i++) {
+            //不断更新当前的最长公共前缀
             str = longestCommonPrefix(str, strs[i]);
         }
         return str;
     }
 
+    //求a和b的最长公共前缀
     private String longestCommonPrefix(String a, String b) {
         int i = 0, j = 0;
         while (j < a.length() && j < b.length()) {
