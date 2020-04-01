418824219507b76c36fb11a024260eaccea110cd
Monster <516094242@qq.com>
2020-04-01 19:51:56 +0800

    547. 朋友圈

--- src/leetcode/FriendCircles.java
+++ src/leetcode/FriendCircles.java
@@ -2,7 +2,7 @@ package leetcode;
 
 /**
  * 547. Friend Circles
- *
+ * 使用并查集或DFS均可
  * @author Monster
  * @date 2017/11/23
  */
@@ -21,6 +21,7 @@ public class FriendCircles {
     }
 
     private void dfs(int[][] M, int[] visited, int i) {
+        //查看i与其他人的关系
         for (int j = 0; j < M.length; j++) {
             if (M[i][j] == 1 && visited[j] == 0) {
                 visited[j] = 1;
