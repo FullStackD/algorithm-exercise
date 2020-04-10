41464aec9c78bbb0958eda7f8c22f24794cede24
Monster <516094242@qq.com>
2020-04-10 09:11:06 +0800

    23. 合并K个排序链表

--- src/leetcode/MergeKSortedLists.java
+++ src/leetcode/MergeKSortedLists.java
@@ -1,39 +1,37 @@
 package leetcode;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
- * Created by Admin on 2017/9/2 0002.
+ * 23. 合并K个排序链表
  */
 public class MergeKSortedLists {
 
+    /**
+     * 借助PriorityQueue
+     */
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
             return null;
         }
-        PriorityQueue<ListNode> queue = new PriorityQueue<>(new Comparator<ListNode>() {
-
-            @Override
-            public int compare(ListNode o1, ListNode o2) {
-                if (o1.val > o2.val) {
-                    return 1;
-                }
-                if (o1.val < o2.val) {
-                    return -1;
-                }
-                return 0;
+        //最小堆（数组实现）
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.val > o2.val) {
+                return 1;
             }
-
+            if (o1.val < o2.val) {
+                return -1;
+            }
+            return 0;
         });
-        ListNode result = new ListNode(0);
-        ListNode tmp = result;
         for (ListNode list : lists) {
             while (list != null) {
                 queue.add(list);
                 list = list.next;
             }
         }
+        ListNode result = new ListNode(0);
+        ListNode tmp = result;
         while (!queue.isEmpty()) {
             tmp.next = queue.poll();
             tmp = tmp.next;
@@ -42,4 +40,45 @@ public class MergeKSortedLists {
         return result.next;
     }
 
+    /**
+     * 分治算法
+     */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        if (lists.length == 1) {
+            return lists[0];
+        }
+        if (lists.length == 2) {
+            return mergeTwoLists(lists[0], lists[1]);
+        }
+        int mid = lists.length / 2;
+        ListNode[] l1 = new ListNode[mid];
+        System.arraycopy(lists, 0, l1, 0, mid);
+        ListNode[] l2 = new ListNode[lists.length - mid];
+        for (int i = mid, j = 0; i < lists.length; i++, j++) {
+            l2[j] = lists[i];
+        }
+        return mergeTwoLists(mergeKLists2(l1), mergeKLists2(l2));
+    }
+
+    //递归合并两个排序链表
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head;
+        if (l1.val <= l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
 }
