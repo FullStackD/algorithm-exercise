f75b7bd8bdb98a0440dabf97235f7a6bb6c0772b
Monster <516094242@qq.com>
2020-04-22 23:56:46 +0800

    148. 排序链表

--- src/leetcode/SortList.java
+++ src/leetcode/SortList.java
@@ -1,7 +1,16 @@
 package leetcode;
 
+/**
+ * 148. 排序链表
+ */
 public class SortList {
 
+
+    /**
+     * 归并排序链表（递归）
+     * O(logn)的空间复杂度
+     * O(nlogn)的时间复杂度
+     */
     public ListNode sortList(ListNode head) {
         //1. 处理异常数据  Null/一个节点
         if (head == null || head.next == null) {
@@ -22,6 +31,7 @@ public class SortList {
         return merge(left, right);
     }
 
+    //合并两个有序链表
     private ListNode merge(ListNode left, ListNode right) {
         if (left == null) {
             return right;
@@ -49,5 +59,60 @@ public class SortList {
         }
         return result.next;
     }
+
+    /**
+     * 归并排序（从底至顶直接合并）
+     */
+    public ListNode sortList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        int len = listNodeLength(head);
+
+        ListNode res = new ListNode(-1);
+        res.next = head;
+
+        for (int i = 1; i < len; i *= 2) {
+            ListNode prev = res;
+            ListNode curr = res.next;
+
+            while (curr != null) {
+                //找到两个待合并链表
+                ListNode left = curr;
+                ListNode right = split(left, i);
+                curr = split(right, i);//移动到下一个归并段的起始位置
+                prev.next = merge(left, right);
+
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+            }
+        }
+        return res.next;
+    }
+
+    private ListNode split(ListNode head, int step) {
+        if (head == null) {
+            return null;
+        }
+        for (int i = 1; head.next != null && i < step; i++) {
+            head = head.next;
+        }
+        ListNode right = head.next;
+        head.next = null;
+        return right;
+    }
+
+    //求链表长度
+    private int listNodeLength(ListNode head) {
+        int length = 0;
+        ListNode t = head;
+        while (t != null) {
+            t = t.next;
+            length++;
+        }
+        return length;
+    }
+
 
 }
