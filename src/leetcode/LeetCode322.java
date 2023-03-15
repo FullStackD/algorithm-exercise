09e3881ad3e1da19bcd2c4fada8390bad9a4c869
Monster <a516094242@gmail.com>
2023-03-15 22:09:13 +0800

    322. 零钱兑换

--- src/leetcode/LeetCode322.java
+++ src/leetcode/LeetCode322.java
@@ -1,28 +1,27 @@
 package leetcode;
 
-import java.util.Arrays;
-
-
 /**
  * 322. 零钱兑换(#)
  * <p>
  * 动态规划（自下而上）
  */
-public class CoinChange {
+public class LeetCode322 {
 
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;//初始最大值，便于后续更新
-        int[] dp = new int[amount + 1];//记录i元最少硬币数
-        Arrays.fill(dp, max);
+        //初始最大值，便于后续更新
+        int max = amount + 1;
+        //记录i元最少硬币数
+        int[] dp = new int[amount + 1];
         dp[0] = 0;
-        for (int i = 1; i < dp.length; i++) {
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = max;
             for (int coin : coins) {
                 if (coin <= i) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == max ? -1 : dp[amount];
     }
 
 
