be25d6f5167b79ef5162d7b09aca32f21ccabf9e
Monster <a516094242@gmail.com>
2023-03-19 20:32:11 +0800

    100. 相同的树

---
+++ src/leetcode/LeetCode100.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import leetcode.common.TreeNode;
+
+/**
+ * 100. 相同的树
+ */
+public class LeetCode100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
