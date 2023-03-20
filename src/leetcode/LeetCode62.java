74e9dfbd9a78acefbf528e8230573e15bc0a5b29
Monster <a516094242@gmail.com>
2023-03-20 23:46:44 +0800

    62. 不同路径

--- src/leetcode/LeetCode62.java
+++ src/leetcode/LeetCode62.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+/**
+ * 62. 不同路径
+ */
 public class LeetCode62 {
 
     /**
@@ -21,6 +24,28 @@ public class LeetCode62 {
             y++;
         }
         return (int) ans;
+    }
+
+    /**
+     * DP解法
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePathsWithDP(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+
     }
 
 }
