ad310a2d6e6eea7d7e47971a349998f3784cc6ba
Monster <a516094242@gmail.com>
2023-04-10 00:35:46 +0800

    49. 字母异位词分组

---
+++ src/leetcode/LeetCode49.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 49. 字母异位词分组
+ */
+public class LeetCode49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] tmpArr = str.toCharArray();
+            Arrays.sort(tmpArr);
+            String key = new String(tmpArr);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> useStream(String[] strs) {
+        return new ArrayList<>(Arrays.stream(strs)
+                .collect(Collectors.groupingBy(str -> {
+                    // 返回 str 排序后的结果。
+                    // 按排序后的结果来grouping by，算子类似于 sql 里的 group by。
+                    char[] array = str.toCharArray();
+                    Arrays.sort(array);
+                    return new String(array);
+                })).values());
+    }
+
+}
