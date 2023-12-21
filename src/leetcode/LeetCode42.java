8f43b7a5b0657ab0ada934c83d0e73d2dab8a19b
Monster <a516094242@gmail.com>
2023-12-21 21:18:46 +0800

    42. 接雨水

--- src/leetcode/LeetCode42.java
+++ src/leetcode/LeetCode42.java
@@ -35,5 +35,25 @@ public class LeetCode42 {
         return sum;
     }
 
+    public int trapV2(int[] height) {
+        // 计算每个位置右边最大值
+        int n = height.length;
+        int[] maxRight = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
+        }
+        int sum = 0;
+        int maxLeft = 0;
+        // 计算每个位置的水量（0和n-1的位置无需计算，不会有水）
+        for (int i = 1; i < n - 1; i++) {
+            maxLeft = Math.max(height[i - 1], maxLeft);
+            int min = Math.min(maxLeft, maxRight[i]);
+            if (min > height[i]) {
+                sum += min - height[i];
+            }
+        }
+        return sum;
+    }
+
 
 }
