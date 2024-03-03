057794b2b12588e93811da436a8fb15e92c39dde
Monster <a516094242@gmail.com>
2024-03-03 13:39:26 +0800

    148. 排序链表

--- src/leetcode/LeetCode148.java
+++ src/leetcode/LeetCode148.java
@@ -1,9 +1,11 @@
 package leetcode;
 
+import leetcode.common.ListNode;
+
 /**
  * 148. 排序链表
  */
-public class SortList {
+public class LeetCode148 {
 
 
     /**
@@ -12,22 +14,22 @@ public class SortList {
      * O(nlogn)的时间复杂度
      */
     public ListNode sortList(ListNode head) {
-        //1. 处理异常数据  Null/一个节点
         if (head == null || head.next == null) {
             return head;
         }
-        //2. 找到链表的中间点
-        ListNode p = head;//慢指针
-        ListNode q = head;//快指针
+
+        ListNode p = head;// 慢指针
+        ListNode q = head;// 快指针
         while (q.next != null && q.next.next != null) {
             p = p.next;
             q = q.next.next;
         }
-        //3. 左边sortList，右边sortList
+        // 右侧链表排序
         ListNode right = sortList(p.next);
+        // 左侧链表排序，断链避免后半部分重新排序
         p.next = null;
         ListNode left = sortList(head);
-        //4. merge函数
+        // 合并两个有序链表
         return merge(left, right);
     }
 
@@ -39,7 +41,7 @@ public class SortList {
         if (right == null) {
             return left;
         }
-        ListNode result = new ListNode(0);
+        ListNode result = new ListNode();
         ListNode p = result;
         while (left != null && right != null) {
             if (left.val > right.val) {
