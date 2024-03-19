dcb6b5b360999ad71fede50596bdd2150cb4096a
Monster <a516094242@gmail.com>
2024-03-19 13:54:44 +0800

    114. 二叉树展开为链表

--- src/leetcode/LeetCode114.java
+++ src/leetcode/LeetCode114.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+
 /**
  * 114. 二叉树展开为链表
  */
@@ -9,36 +10,25 @@ public class LeetCode114 {
         if (root == null) {
             return;
         }
-        TreeNode leftResult = flattenTree(root.left);
-        TreeNode rightResult = flattenTree(root.right);
-        if (leftResult != null) {
-            root.right = leftResult;
-            while (leftResult.right != null) {
-                leftResult = leftResult.right;
-            }
-            leftResult.right = rightResult;
-        } else {
-            root.left = null;
-            root.right = rightResult;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        // 展开后的单链表是先序遍历，所以保存的left和right就是头结点
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 左子树指向null
+        root.left = null;
+        root.right = left;
+
+        // 找到加上左子树后，链表的最右下的节点
+        TreeNode rightMost = root;
+        while (rightMost.right != null) {
+            rightMost = rightMost.right;
         }
+        rightMost.right = right;
     }
 
-    private TreeNode flattenTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        TreeNode leftResult = flattenTree(root.left);
-        TreeNode rightResult = flattenTree(root.right);
-        if (leftResult != null) {
-            root.right = leftResult;
-            while (leftResult.right != null) {
-                leftResult = leftResult.right;
-            }
-            leftResult.right = rightResult;
-        } else {
-            root.right = rightResult;
-        }
-        return root;
-    }
 
 }
