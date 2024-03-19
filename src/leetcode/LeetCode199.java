fe22daf7793358ab3c405822286e3282de511ee1
Monster <a516094242@gmail.com>
2024-03-19 12:54:29 +0800

    199. 二叉树的右视图

---
+++ src/leetcode/LeetCode199.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 199. 二叉树的右视图
+ */
+public class LeetCode199 {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode t = queue.poll();
+                if (t.left != null) {
+                    queue.add(t.left);
+                }
+                if (t.right != null) {
+                    queue.add(t.right);
+                }
+                if (i == len - 1) {
+                    result.add(t.val);
+                }
+            }
+        }
+        return result;
+    }
+
+}
