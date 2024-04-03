a609a6fd2c5e2f10cbec22d848720cd0f435e61e
Monster <a516094242@gmail.com>
2024-04-03 22:20:17 +0800

    146. LRU缓存机制

--- src/leetcode/LeetCode146.java
+++ src/leetcode/LeetCode146.java
@@ -13,7 +13,7 @@ public class LeetCode146 {
         private Map<Integer, Integer> map;
 
         public LRUCache(int capacity) {
-            map = new LinkedHashMap(capacity, 0.75f, true) {
+            map = new LinkedHashMap<>(capacity, 0.75f, true) {
                 @Override
                 protected boolean removeEldestEntry(Map.Entry eldest) {
                     return map.size() > capacity;
